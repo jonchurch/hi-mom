cbe317bb5cb490678bcdbfa68609a48a489a9809
jonchurch <me@jonchurch.com>
2018-02-21 17:07:17 +0700

    add moment js to locals

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -39,6 +40,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.local.moment = moment
 
   // render the error page
   res.status(err.status || 500);
